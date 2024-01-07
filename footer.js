5af10ad0bf3a58fad2bb85b21bee55dbc16fbcae
Tushar Patle <53601925+tushar-patle@users.noreply.github.com>
2024-01-07 15:22:30 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,6 @@
+// Create a footer element
+const footer = document.createElement('footer');
+footer.innerHTML = '<p>&copy; 2024 Jokes&Pun. All rights reserved.</p>';
+
+// Append the footer to the body
+document.body.appendChild(footer);
